6026b5ee75c62f3fb9ff770d09dc7a792d5b5a45
rhenium <rhenium750@gmail.com>
2022-01-20 19:54:21 +0900

    エラーするので一旦静的ファイルに

--- src/lib/devConsole/main.tsx
+++ src/lib/devConsole/main.tsx
@@ -1,25 +1,18 @@
 import React from "react";
 import express from "express";
-import DownloadPage from "./pages/DownloadPage";
-import { render } from "./Rendering";
 import NeosObject from "NeosObject";
+const path = require("path");
 
 const server = express();
 const port = process.env.PORT || 3000;
 const startedTime = new Date();
 
-server.get("/", (_req, res) => {
-  const page = render(<DownloadPage />, {
-    title: "neoscript console",
-    description: "neoscriptで生成したアイテムをダウンロードできます。",
-  });
-  res.status(200).send(page);
-});
+server.use(express.static(path.join(__dirname, "public")));
 
 server.get("/api/obj.json", (req, res) => {
   const { p: params } = req.query ?? {};
 
-  res.json(JSON.parse(NeosObject.toNeosObject({ Size: [1, 0.5, 1] })));
+  res.json(JSON.parse(NeosObject.toNeosObject(JSON.parse(params ?? "{}"))));
 });
 
 server.get("/api/start_time", (req, res) => {
