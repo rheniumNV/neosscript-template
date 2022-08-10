d35b4b1404401c27e27c40dbdec19485b0c3cce8
sweshelo <sweshelo@gmail.com>
2022-08-11 03:18:29 +0900

    [fix] 型修正 / 型追加

--- src/lib/core/types/NeosType.tsx
+++ src/lib/core/types/NeosType.tsx
@@ -1,7 +1,9 @@
 export type NEOS_TYPE = { name: string };
 export default {
-  Bool: { name: "[System.Bool, mscorlib]" },
+  Bool: { name: "[System.Boolean, mscorlib]" },
   Int: { name: "[System.Int32, mscorlib]" },
+  Int2: { name: "[BaseX.int2, BaseX]" },
+  Int3: { name: "[BaseX.int3, BaseX]" },
   Float: { name: "[System.Single, mscorlib]" },
   Float2: { name: "[BaseX.float2, BaseX]" },
   Float3: { name: "[BaseX.float3, BaseX]" },
@@ -9,4 +11,5 @@ export default {
   Uri: { name: "[System.Uri, System]" },
   Color: { name: "[BaseX.color, BaseX]" },
   Slot: { name: "[FrooxEngine.Slot, FrooxEngine]" },
+  User: { name: "[FrooxEngine.User, FrooxEngine]" },
 };
